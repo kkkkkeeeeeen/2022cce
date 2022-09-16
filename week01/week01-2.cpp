9f16b53a27216a27e291b69744b38eefc3dd74e5
kkkkkeeeeeen <113664200+kkkkkeeeeeen@users.noreply.github.com>
2022-09-16 14:42:59 +0800

    week01

---
+++ week01/week01-2.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("Hollow World\n");
+printf("Hollow World\n");
+printf("我是小葉老師\n");
+}
---
+++ week01/wwek01-1.cpp
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+printf("Hollow World\n");
+}
