ef0fb5b61328061d7d3ff98de0e46fdb14d4f8c7
ken <113664200+kkkkkeeeeeen@users.noreply.github.com>
2022-11-14 22:15:51 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("請輸入兩個字: ");
+scanf("%d%d",&a,&b);
+printf("%d+%d是%d\n",a,b,a+b);
+printf("%d-%d是%d\n",a,b,a-b);
+printf("%d*%d是%d\n",a,b,a+b);
+printf("%d/%d是%d\n",a,b,a/b);
+printf("%d%%d的餘數是%d\n",a,b,a%b);
+return 0;
+}
